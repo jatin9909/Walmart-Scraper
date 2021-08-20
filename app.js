b70e170deff0fd936e7630775dc406891782db92
Jatin <jatin.dhar27@gmail.com>
2021-08-20 12:39:20 +0530

    app ready for deployed

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.use((req, res, next) => {
 	next();
 });
 
-//setting middleware for method override
+//setting middl eware for method override
 app.use(methodOverride('_method'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
